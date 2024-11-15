f6f611401c289fbd52f3cc44ba4afdffd035f94d
REF2024 <REF2024@PL40>
2024-11-15 12:21:51 +0800

    Add project files.

---
+++ MilanteDB/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Segoe UI", 12F)
+        Label1.ForeColor = SystemColors.ActiveCaptionText
+        Label1.Location = New Point(133, 211)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(81, 21)
+        Label1.TabIndex = 1
+        Label1.Text = "Username"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI", 12F)
+        Label2.ForeColor = Color.Black
+        Label2.Location = New Point(133, 257)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(76, 21)
+        Label2.TabIndex = 2
+        Label2.Text = "Password"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.GradientInactiveCaption
+        TextBox1.Location = New Point(246, 211)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = SystemColors.GradientInactiveCaption
+        TextBox2.Location = New Point(246, 257)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PasswordChar = "*"c
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Tahoma", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(51, 153)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(74, 23)
+        Label3.TabIndex = 7
+        Label3.Text = "LOGIN"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(137, 311)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(209, 32)
+        Button1.TabIndex = 8
+        Button1.Text = "LOGIN"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(137, 362)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(209, 32)
+        Button2.TabIndex = 9
+        Button2.Text = "CHECK CONNECTION"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonFace
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(986, 616)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ MilanteDB/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    ' MySQL connection details
+    Dim conn As New MySqlConnection("server=your_server;userid=your_userid;password=your_password;database=matt")
+    Dim COMMAND As MySqlCommand
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Check if both username and password fields are filled
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost;userid=root;password='';database=matt"
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "select * from matt.employee where UserName = '" & TextBox1.Text & "' and PW = '" & TextBox2.Text & "' "
+            COMMAND = New MySqlCommand(Query, conn)
+            READER = COMMAND.ExecuteReader
+            Dim count As Integer
+            count = 0
+            While READER.Read
+                count = count + 1
+            End While
+
+            If count = 1 Then
+                MessageBox.Show("Username and password are Correct")
+            ElseIf count > 1 Then
+                MessageBox.Show("Username and password are Duplicate")
+            Else
+                MessageBox.Show("Username and password are incorrect")
+
+            End If
+
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        ' Code for label click, if needed
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Any initialization code when the form loads
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        ' Any logic to handle text changes in TextBox1 (if needed)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost;userid=root;password='';database=test"
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING      CONNECTION TO MySQL DATABASE")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+End Class
